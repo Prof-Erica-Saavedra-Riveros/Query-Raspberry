a6a7ff3f32fcf4b756ca1fbc23759316a90dec39
Erica Saavedra Riveros <106543453+Prof-Erica-Saavedra-Riveros@users.noreply.github.com>
2023-01-02 14:06:24 -0600

    Update Query-Rasp.py

--- Query-Rasp.py
+++ Query-Rasp.py
@@ -1,1 +1,17 @@
 
+import mysql.connector
+
+cnx = mysql.connector.connect(user='adrianlan', password='1234',
+                                 host='192.168.100.158',
+                                 database='coviddetector')
+cursor = cnx.cursor()
+query = ("SELECT id, nombre, temp FROM registro WHERE temp>40")
+
+
+cursor.execute(query)
+
+for (id, nombre, temp) in cursor:
+  print(id, nombre, temp)
+
+cursor.close()
+cnx.close()
